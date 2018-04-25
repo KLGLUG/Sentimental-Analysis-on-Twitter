d11cc494771375ff339a9a8f0d9494f48887e1d9
Farhaan Ahmed <farhaanfsk@gmail.com>
2018-04-25 19:41:19 +0530

    Update senti_with_avg_tfidf_word2vec.py

--- senti_with_avg_tfidf_word2vec.py
+++ senti_with_avg_tfidf_word2vec.py
@@ -1,4 +1,23 @@
-# Natural Language Processing
+;;; Sentiment Analysis on Twitter using Machine learnig
+;;; Copyright © 2018 Uday Kiran Kondreddy <udaykiran.kondreddy@gmail.com>
+;;; Copyright © 2018 Farhaan Ahmed Shaik <farhaanfsk@gmail.com>
+;;; Copyright © 2018 Subhani Kurra <subhanikurra4@gmail.com>
+;;; Copyright © 2018 Naga Teja Mamidapaka <nagatejam@gmail.com>
+;;;
+;;; This file is part of Sentimental Analysis on Twitter.
+;;;
+;;; This is free Code; you can redistribute it and/or modify it
+;;; under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 3 of the License, or (at
+;;; your option) any later version.
+;;;
+;;; This code is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
 
 # Importing the libraries
 import numpy as np
