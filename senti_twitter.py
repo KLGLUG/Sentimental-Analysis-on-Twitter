c41ed2835b0176271348aeb71a9f0de4bd89bb0d
farhaanfsk <farhaanfsk@gmail.com>
2018-05-09 23:29:19 +0530

    lexican based approach

--- senti_twitter.py
+++ senti_twitter.py
@@ -1,3 +1,24 @@
+'''
+;;; Multi_class Sentiment Analysis on Twitter using Machine learning model
+;;; Copyright © 2018 Uday Kiran Kondreddy <udaykiran.kondreddy@gmail.com>
+;;; Copyright © 2018 Farhaan Ahmed Shaik <farhaanfsk@gmail.com>
+;;; Copyright © 2018 Subhani Kurra <subhanikurra4@gmail.com>
+;;; Copyright © 2018 Naga Teja Mamidapaka <nagatejam@gmail.com>
+;;;
+;;; This file is part of Sentimental Analysis on Twitter.
+;;;
+;;; This is free Code; you can redistribute it and/or modify it
+;;; under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 3 of the License, or (at
+;;; your option) any later version.
+;;;
+;;; This code is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; See <http://www.gnu.org/licenses/>.'''
+
 import re
 import tweepy
 from tweepy import OAuthHandler
